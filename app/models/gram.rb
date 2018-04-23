a901bc2f77cb1b45251c2d6421d1783e57b1360f
Bryan Jimenez <bryan@roguedevs.io>
2018-04-23 17:29:48 +0000

    Added comments function and validation tests

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,10 @@
 class Gram < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
   validates :message, presence: true
   validates :picture, presence: true
 
   mount_uploader :picture, PictureUploader
-  belongs_to :user
+
 end
